922f8d2d517b0ae292959e80dc411d99591d7140
pardal-progresource <pardal.piu@gmail.com>
2025-08-01 00:49:40 +0100

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,12 @@
 <template>
+  <div>
+    <nav class="bg-green-700 text-white p-4 flex gap-4">
+      <router-link to="/">Início</router-link>
+      <router-link to="/equipments">Equipamentos</router-link>
+    </nav>
+
   <router-view />
+</div>
 </template>
 
 <script>
