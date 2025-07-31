a1834fff72363ed087fa6ca100fb46b435d4c3a3
pardal-progresource <pardal.piu@gmail.com>
2025-07-31 16:38:27 +0100

    Create Dashboard.vue

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold mb-4">Dashboard</h1>
+    <p>Bem-vindo ao painel de gestão de equipamentos.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard'
+};
+</script>
