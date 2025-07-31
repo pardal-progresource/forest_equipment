6f5a09002044d4c7f5341e4fa11a0ac94b66b908
pardal-progresource <pardal.piu@gmail.com>
2025-07-31 20:08:44 +0100

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,13 +4,18 @@ import Login from '../views/Login.vue';
 import Dashboard from '../views/Dashboard.vue';
 import Equipments from '../views/Equipments.vue';
 import MyLoans from '../views/MyLoans.vue';
+import AddEquipment from '../views/AddEquipment.vue';  // importação do novo componente
 
 const routes = [
   { path: '/', component: Home },
   { path: '/login', component: Login },
   { path: '/dashboard', component: Dashboard },
   { path: '/equipamentos', component: Equipments },
+  { path: '/equipamentos/novo', component: AddEquipment },  // nova rota para criar equipamento
   { path: '/meus-empréstimos', component: MyLoans }
 ];
 
-export default createRouter({ history: createWebHistory(), routes });
+export default createRouter({
+  history: createWebHistory(),
+  routes,
+});
