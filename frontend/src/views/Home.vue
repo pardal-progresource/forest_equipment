8ac1ab5f0648f223637fc18943fde93051aae4d4
pardal-progresource <pardal.piu@gmail.com>
2025-08-01 00:53:14 +0100

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,7 @@
 <!-- frontend/src/views/Home.vue -->
 <template>
   <div class="p-8 text-center">
-    <h1 class="text-3xl font-bold mb-4 text-green-700">Bem-vindo à PLataforma de Gestão de Equipamentos de Inventário Florestal</h1>
+    <h1 class="text-3xl font-bold mb-4 text-green-700">Bem-vindo à Plataforma de Gestão de Equipamentos de Inventário Florestal</h1>
     <p class="text-gray-700">
       Utilize o menu de navegação acima para visualizar e gerir os equipamentos florestais disponíveis.
     </p>
