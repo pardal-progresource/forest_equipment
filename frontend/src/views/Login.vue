ffc6bf5777fed7ceb4bcc84f512307bda6de95fa
pardal-progresource <pardal.piu@gmail.com>
2025-07-31 15:07:58 +0100

    Update Login.vue

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -9,15 +9,16 @@
   </div>
 </template>
 <script>
-import axios from 'axios';
+import api from '../api';
+
 export default {
   data: () => ({ email: '', password: '' }),
   methods: {
     async login() {
-      const res = await axios.post(`${import.meta.env.VITE_API_URL}/auth/login`, {
-  email: this.email,
-  password: this.password
-});
+      const res = await api.post('/auth/login', {
+        email: this.email,
+        password: this.password
+      });
       localStorage.setItem('token', res.data.token);
       this.$router.push('/dashboard');
     }
